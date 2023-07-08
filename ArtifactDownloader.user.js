be8b371388d785cd6287137dfb5f34d80a9b4457
Rushi Ranpise <37355997+rushiranpise@users.noreply.github.com>
2023-07-08 21:41:06 +0530

    v1.1
    Add user confirmation before redirect

--- ArtifactDownloader.user.js
+++ ArtifactDownloader.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         Artifact Downloader
 // @namespace    rushiranpise
-// @version      1.0
+// @version      1.1
 // @description  Downloads GitHub action artifacts without login using nightly.link
 // @author       rushiranpise
-// @match        https://github.com/*
+// @match        https://github.com/*/*/actions/*
+// @match        https://github.com/*/*/actions
 // @grant        none
 // ==/UserScript==
 
@@ -13,6 +14,9 @@
 
     // Function to handle the URL redirection
     function redirectGitHubUrl(url) {
+    // Check if the current page ends with "actions"
+    const isActionsPage = window.location.href.endsWith('/actions');
+
         // Extract the username, repo, and run ID from the URL
         const matchResult = url.match(/https:\/\/github\.com\/([^/]+)\/([^/]+)\/actions\/runs\/([^/]+)/);
         if (matchResult) {
@@ -21,10 +25,18 @@
             // Build the new URL
             const newUrl = `https://nightly.link/${username}/${repo}/actions/runs/${runId}`;
 
+        // Ask for confirmation before redirecting
+        const confirmation = confirm(`Do you want to redirect to ${newUrl}?`);
+        if (confirmation) {
             // Redirect to the new URL
             window.location.replace(newUrl);
+        } else if (isActionsPage) {
+            // Continue with the original link if on the actions page
+            window.location.href = url;
         }
     }
+}
+
 
     // Event listener for click events on run links
     document.addEventListener('click', function(event) {
